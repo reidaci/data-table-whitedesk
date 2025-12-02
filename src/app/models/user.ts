0324ab2d43d2133125aa64898227a695349cac70
Reiz Daci <reiz.daci@outlook.com>
2025-12-02 20:29:24 +0100

    restructure User model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,24 +1,26 @@
 export interface User {
-    id: number;
-    name:string;
-    username:string;
-    email:string;
-    phone:string;
-    website:string;
-    address:{
-        street:string;
-        suite:string;
-        city:string;
-        zipcode:string;
-        geo:{
-            lat:string;
-            lng:string;
-        };
-    };
-    company:{
-        name:string;
-        catchPhrase:string;
-        bs:string
-    }
-
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: Adress;
+  company: Company;
+}
+export interface Adress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+export interface Geo {
+  lat: string;
+  lng: string;
 }
